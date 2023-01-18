d125fef313161d4e5f6fcb062451c8052e7ed81b
fenil07 <fenilgohil124@gmail.com>
2023-01-18 18:14:16 +0530

    grid view added

--- lib/home.dart
+++ lib/home.dart
@@ -19,44 +19,51 @@ class _HomeState extends State<Home> {
     Colors.purpleAccent,
     Colors.brown];
     return Scaffold(
-
-      body: GridView.count(crossAxisCount: 2,
-    //  mainAxisSpacing: 11,
-    //  crossAxisSpacing: 11,
-      children: [
-        Padding(
+    //   body: GridView.count(crossAxisCount: 2,
+    // //  mainAxisSpacing: 11,
+    // //  crossAxisSpacing: 11,
+    //   children: [
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[0],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[1],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[2],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[3],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[4],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[5],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[6],),
+    //     ),
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Container(color: arrColors[7],),
+    //     ),
+    //     ],),
+      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount
+        (crossAxisCount: 2/*,mainAxisSpacing: 11,crossAxisSpacing: 11*/),
+        itemBuilder: (context, index) {
+        return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[0],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[1],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[2],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[3],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[4],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[5],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[6],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(color: arrColors[7],),
-        ),
-        ],),
+          child: Container(color: arrColors[index],),
+        );
+      },itemCount: arrColors.length,),
     );
   }
 }
